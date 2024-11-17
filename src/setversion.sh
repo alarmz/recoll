9ebd9072ce232fd6a57f9a3ac6d7033c35620067
Jean-Francois Dockes <jf@dockes.org>
2024-11-17 08:44:32 +0100

    version: update setversion.sh to also set it in appdata

--- src/setversion.sh
+++ src/setversion.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 VERSION=`cat RECOLL-VERSION.txt`
+DATE=`ls --time-style=long-iso -l RECOLL-VERSION.txt | awk '{print $6}'`
 
 sed -i -E -e '/^#define[ \t]+PACKAGE_VERSION/c\'\
 "#define PACKAGE_VERSION \"$VERSION\"" \
@@ -9,3 +10,7 @@ common/autoconfig-win.h common/autoconfig-mac.h
 sed -i -E -e '/VERSIONCOMMENT/c\'\
 "    version: '$VERSION', # VERSIONCOMMENT keep this here, used by setversion.sh" \
 meson.build
+
+sed -i -E -e '/<release version=/c\'\
+"    <release version=\"$VERSION\" date=\"$DATE\" />" \
+desktop/org.recoll.recoll.appdata.xml
