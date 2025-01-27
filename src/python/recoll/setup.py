149725a413dfc7089591a63b03f854edebea2889
Jean-Francois Dockes <jf@dockes.org>
2025-01-27 16:39:00 +0100

    recoll python ext.: experimented with pyproject.toml "build". Not satisfactory (see comments in setup.py). Used neither on Linux (meson) nor Windows (directly exec setup-win.py).

--- src/python/recoll/setup.py
+++ src/python/recoll/setup.py
@@ -1,49 +1,47 @@
 # Semi-generic version of setup.py. This is not used anymore on ux systems (which use meson instead)
 # Use as a base if you need another kind of build. Supposes that librecoll and the include files are
 # available somewhere
-# After customizing, install the wheel module with pip and run python3 setup.py bdist_wheel to
-# obtain an installable .whl
+# After customizing (esp: the absolute path to the source tree), this should be usable with
+# "python3 -m build" or such.
 
 from setuptools import setup, Extension
 import os
-import sys
 
 # For shadow builds: references to the source tree
-pytop = '/home/dockes/projets/fulltext/recoll/src/python/recoll'
-# Need to set this. Where will the default config be stored. e.g. /usr/share/recoll on linux
-RECOLL_DATADIR = '/usr/share/recoll'
+# 
+# This happens with e.g. "python3 -m build". The setup.py file is copied to a temporary directory
+# and there seems to be no way at all to refer the original source tree.
+#
+# I could find no way to use ../../RECOLL-VERSION.txt
+# Adding it as an include to MANIFEST.in does not seem to do anything (it appears to only work for
+# files inside the python package source tree). So hard-code the source path:
+recoll = "/home/dockes/projets/fulltext/recoll/src/"
 
-top = os.path.join(pytop, '..', '..')
+VERSION = open(os.path.join(recoll, "RECOLL-VERSION.txt")).read().strip()
 
-VERSION = open(os.path.join(top, "RECOLL-VERSION.txt")).read().strip()
-
-# For shadow builds: reference to the top of the local tree (for finding
-# generated .h files, e.g. autoconfig.h)
-localtop = os.path.join(os.path.dirname(__file__), '..', '..')
-
-    
 extra_compile_args = ['-std=c++17']
+
+# Need to set this. Where will the default config be stored. e.g. /usr/share/recoll on linux. The
+# Windows code finds it relative to the exec.
+RECOLL_DATADIR = '/usr/share/recoll'
 define_macros = [('RECOLL_DATADIR', '"' + RECOLL_DATADIR + '"'),]
+
 # Set to 1 depending on what is set in the main build (this is a meson option set to off by default)
 DEF_EXT4_BIRTH_TIME = 0
 if DEF_EXT4_BIRTH_TIME == 1:
    define_macros.append(('EXT4_BIRTH_TIME', 1))
 
-
-# See setup.cfg. Not needed with older python3 versions. no idea, but needed else bdist_wheel fails.
-if not os.path.exists('Recoll.egg-info'):
-   os.mkdir('Recoll.egg-info')
-   
-# These are only needed if building with a full source tree and the includes are not installed
+# include directories for building without a -dev package installed
 include_dirs = [
-        os.path.join(localtop, 'common'),
-        os.path.join(top, 'common'), 
-        os.path.join(top, 'index'), 
-        os.path.join(top, 'internfile'), 
-        os.path.join(top, 'query'), 
-        os.path.join(top, 'rcldb'), 
-        os.path.join(top, 'utils'), 
+   os.path.join(recoll, 'common'),
+   os.path.join(recoll, 'index'),
+   os.path.join(recoll, 'internfile'),
+   os.path.join(recoll, 'query'),
+   os.path.join(recoll, 'rcldb'),
+   os.path.join(recoll, 'utils'),
+   os.path.join(recoll, 'python', 'recoll'),
 ]
+
 # Use the standard location for installed includes (Linux, mostly).
 include_dirs.append('/usr/include/recoll')
 
@@ -64,21 +62,14 @@ module1 = Extension('_recoll',
                     libraries = libraries,
                     library_dirs = library_dirs,
                     runtime_library_dirs = runtime_library_dirs,
-                    sources = [os.path.join(pytop, 'pyrecoll.cpp'),
-                               os.path.join(pytop, 'pyresultstore.cpp'),
-                               os.path.join(pytop, 'pyrclextract.cpp')
+                    sources = ['pyrecoll.cpp',
+                               'pyresultstore.cpp',
+                               'pyrclextract.cpp'
                                ])
 
 setup (name = 'Recoll',
        version = VERSION,
-       description = 'Query/Augment a Recoll full text index',
-       author = 'J.F. Dockes',
-       author_email = 'jfd@recoll.org',
-       url = 'https://www.recoll.org',
-       license = 'GPL',
-       package_dir = {'' : os.path.join(top, 'python', 'recoll')},
-       long_description = '''
-''',
        packages = ['recoll'],
        ext_package = 'recoll',
-       ext_modules = [module1])
+       ext_modules = [module1]
+       )
