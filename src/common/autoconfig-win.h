14c7c75912160c76454ddf807e0719b1c0eaa920
Jean-Francois Dockes <jf@dockes.org>
2025-10-06 20:37:27 +0200

    bump version

--- src/common/autoconfig-win.h
+++ src/common/autoconfig-win.h
@@ -17,7 +17,7 @@
 #undef IDX_THREADS
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.43.5"
+#define PACKAGE_VERSION "1.43.6"
 
 /* Use QTextBrowser to implement the preview windows */
 #undef PREVIEW_FORCETEXTBROWSER
